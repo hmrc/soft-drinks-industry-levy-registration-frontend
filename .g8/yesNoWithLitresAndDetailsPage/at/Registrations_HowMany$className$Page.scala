79dc61b4fc819d8c7147c001e85158d17fc7d28f
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-05-25 21:33:12 +0100

    DLS

--- .g8/yesNoWithLitresAndDetailsPage/at/Registrations_HowMany$className$Page.scala
+++ .g8/yesNoWithLitresAndDetailsPage/at/Registrations_HowMany$className$Page.scala
@@ -8,19 +8,19 @@ package uk.gov.hmrc.test.sdil.pages.returns
 import uk.gov.hmrc.test.sdil.conf.TestConfiguration
 import uk.gov.hmrc.test.sdil.pages.generic.BasePage
 
-object $className$Page extends BasePage {
+object Registrations_HowMany$className$Page extends BasePage {
 
-  override val url: String = TestConfiguration.url("registration-frontend") + "/$yesNoUrl$"
-  override val title = "$yesNoTitle$"
+  override val url: String = TestConfiguration.url("registration-frontend") + "/$litresUrl$"
+  override val title = "$litresTitle$"
 
   override def expectedPageErrorTitle: Option[String] = Some("")
 
   override def expectedPageTitle: Option[String] = Some(
-    "$yesNoTitle$ - Soft Drinks Industry Levy - GOV.UK"
+    "$litresTitle$ - Soft Drinks Industry Levy - GOV.UK"
   )
 
   override def expectedPageHeader: Option[String] = Some(
-    "$yesNoHeading$"
+    "$litresHeading$"
   )
 
 }
