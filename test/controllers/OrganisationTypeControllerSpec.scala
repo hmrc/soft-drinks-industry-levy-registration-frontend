2f2dcefd778ca5354dc162d8c170978168ed7ba2
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-05-25 14:24:18 +0100

    DLS-7693 add files for Organisation Type

---
+++ test/controllers/OrganisationTypeControllerSpec.scala
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import errors.SessionDatabaseInsertError
+import forms.OrganisationTypeFormProvider
+import helpers.LoggerHelper
+import models.{NormalMode, OrganisationType, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.jsoup.Jsoup
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.OrganisationTypePage
+import play.api.data.Form
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.SessionService
+import utilities.GenericLogger
+import views.html.OrganisationTypeView
+
+import scala.concurrent.Future
+
+class OrganisationTypeControllerSpec extends SpecBase with MockitoSugar with LoggerHelper {
+
+  def onwardRoute: Call = Call("GET", "/foo")
+
+  lazy val organisationTypeRoute: String = routes.OrganisationTypeController.onPageLoad(NormalMode).url
+
+  val formProvider = new OrganisationTypeFormProvider()
+  val form: Form[OrganisationType] = formProvider()
+  val withoutSoleTrader: Boolean = false
+  "OrganisationType Controller" - {
+
+    "must return OK and the correct view with 4 radio buttons for a GET if CTEnrollment is true" in {
+
+      val application = applicationBuilder(hasCTEnrolment = true, userAnswers = Some(emptyUserAnswers)).build()
+      running(application) {
+        val request = FakeRequest(GET, organisationTypeRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[OrganisationTypeView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, withoutSoleTrader = true)(request, messages(application)).toString
+      }
+    }
+
+    "must return OK and the correct view with 5 radio buttons for a GET if CTEnrollment is false" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, organisationTypeRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[OrganisationTypeView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, withoutSoleTrader = false)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = UserAnswers(sdilNumber).set(OrganisationTypePage, OrganisationType.values.head).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, organisationTypeRoute)
+
+        val view = application.injector.instanceOf[OrganisationTypeView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(OrganisationType.values.head), NormalMode, withoutSoleTrader)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionService = mock[SessionService]
+
+      when(mockSessionService.set(any())) thenReturn Future.successful(Right(true))
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionService].toInstance(mockSessionService)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, organisationTypeRoute)
+            .withFormUrlEncodedBody(("value", OrganisationType.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, organisationTypeRoute)
+            .withFormUrlEncodedBody(("value", "invalid value"))
+
+        val boundForm = form.bind(Map("value" -> "invalid value"))
+
+        val view = application.injector.instanceOf[OrganisationTypeView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode, withoutSoleTrader)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, organisationTypeRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, organisationTypeRoute)
+            .withFormUrlEncodedBody(("value", OrganisationType.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must fail if the setting of userAnswers fails" in {
+
+      val application = applicationBuilder(userAnswers = Some(userDetailsWithSetMethodsReturningFailure)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, organisationTypeRoute
+        )
+        .withFormUrlEncodedBody(("value", OrganisationType.values.head.toString))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual INTERNAL_SERVER_ERROR
+        val page = Jsoup.parse(contentAsString(result))
+        page.title() mustBe "Sorry, we are experiencing technical difficulties - 500 - soft-drinks-industry-levy - GOV.UK"
+      }
+    }
+
+    "should log an error message when internal server error is returned when user answers are not set in session repository" in {
+      val mockSessionService = mock[SessionService]
+
+      when(mockSessionService.set(any())) thenReturn Future.successful(Left(SessionDatabaseInsertError))
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(
+            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
+            bind[SessionService].toInstance(mockSessionService)
+          )
+          .build()
+
+      running(application) {
+        withCaptureOfLoggingFrom(application.injector.instanceOf[GenericLogger].logger) { events =>
+          val request =
+            FakeRequest(POST, organisationTypeRoute)
+              .withFormUrlEncodedBody(("value", OrganisationType.values.head.toString))
+
+          await(route(application, request).value)
+          events.collectFirst {
+            case event =>
+              event.getLevel.levelStr mustBe "ERROR"
+              event.getMessage mustEqual "Failed to set value in session repository while attempting set on organisationType"
+          }.getOrElse(fail("No logging captured"))
+        }
+      }
+    }
+  }
+}
---
+++ test/views/OrganisationTypeViewSpec.scala
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import controllers.routes
+import forms.OrganisationTypeFormProvider
+import models.{CheckMode, NormalMode, OrganisationType}
+import play.api.data.Form
+import play.api.i18n.Messages
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import views.html.OrganisationTypeView
+
+class OrganisationTypeViewSpec extends ViewSpecHelper {
+
+  val view: OrganisationTypeView = application.injector.instanceOf[OrganisationTypeView]
+  val formProvider = new OrganisationTypeFormProvider
+  val form: Form[OrganisationType] = formProvider.apply()
+  implicit val request: Request[_] = FakeRequest()
+  val withoutSoleTrader: Boolean = true
+
+  object Selectors {
+    val heading = "govuk-fieldset__heading"
+    val legend = "govuk-fieldset__legend  govuk-fieldset__legend--m"
+    val radios = "govuk-radios"
+    val radiosInput = "govuk-radios__input"
+    val radiosItems = "govuk-radios__item"
+    val radiosLabels = "govuk-label govuk-radios__label"
+    val body = "govuk-body"
+    val errorSummaryTitle = "govuk-error-summary__title"
+    val errorSummaryList = "govuk-list govuk-error-summary__list"
+    val button = "govuk-button"
+    val form = "form"
+  }
+
+  "View when withCTEnrollment is false" - {
+    val html = view(form, NormalMode, withoutSoleTrader = false)(request, messages(application))
+    val document = doc(html)
+    "should contain the expected title" in {
+      document.title() must include("Which of these best describes your business?")
+    }
+
+    "should include a legend with the expected heading" in {
+      val legend = document.getElementsByClass(Selectors.legend)
+      legend.size() mustBe 1
+      legend.get(0).getElementsByClass(Selectors.heading).text() mustEqual Messages("Which of these best describes your business?")
+    }
+
+    "when the form is not preoccupied and has no errors" - {
+
+      "should include the expected radio buttons" - {
+        val radiobuttons = document.getElementsByClass(Selectors.radiosItems)
+
+        "that has 5 items" in {
+          radiobuttons.size() mustBe 5
+        }
+        OrganisationType.valuesWithST.zipWithIndex.foreach { case (radio, index) =>
+          s"that has the " + radio.toString + " to select and is unchecked" in {
+            val radio1 = radiobuttons
+              .get(index)
+            radio1
+              .getElementsByClass(Selectors.radiosLabels)
+              .text() mustBe Messages("organisationType." + radio.toString)
+            val input = radio1
+              .getElementsByClass(Selectors.radiosInput)
+            input.attr("value") mustBe radio.toString
+            input.hasAttr("checked") mustBe false
+          }
+        }
+      }
+    }
+
+    "View when withCTEnrollment is true" - {
+      val html = view(form, NormalMode, withoutSoleTrader)(request, messages(application))
+      val document = doc(html)
+      "when the form is not preoccupied and has no errors" - {
+
+        "should include the expected radio buttons" - {
+          val radiobuttons = document.getElementsByClass(Selectors.radiosItems)
+
+          "that has 4 items" in {
+            radiobuttons.size() mustBe 4
+          }
+
+          OrganisationType.values.zipWithIndex.foreach { case (radio, index) =>
+            s"that has the " + radio.toString + " to select and is unchecked" in {
+              val radio1 = radiobuttons
+                .get(index)
+              radio1
+                .getElementsByClass(Selectors.radiosLabels)
+                .text() mustBe Messages("organisationType." + radio.toString)
+              val input = radio1
+                .getElementsByClass(Selectors.radiosInput)
+              input.attr("value") mustBe radio.toString
+              input.hasAttr("checked") mustBe false
+            }
+          }
+        }
+      }
+    }
+
+    OrganisationType.values.foreach { radio =>
+      val html1 = view(form.fill(radio), NormalMode, withoutSoleTrader)(request, messages(application))
+      val document1 = doc(html1)
+
+      s"when the form is preoccupied with " + radio.toString + "selected and has no errors" - {
+        "should have radio buttons" - {
+          val radiobuttons = document1.getElementsByClass(Selectors.radiosItems)
+          OrganisationType.values.zipWithIndex.foreach { case (radio1, index) =>
+            if (radio1.toString == radio.toString) {
+              s"that has the option to select" + radio1.toString + " and is checked" in {
+                val radiobuttons1 = radiobuttons
+                  .get(index)
+                radiobuttons1
+                  .getElementsByClass(Selectors.radiosLabels)
+                  .text() mustBe Messages("organisationType." + radio1.toString)
+                val input = radiobuttons1
+                  .getElementsByClass(Selectors.radiosInput)
+                input.attr("value") mustBe radio1.toString
+                input.hasAttr("checked") mustBe true
+              }
+            } else {
+              s"that has the option to select " + radio1.toString + " and is unchecked" in {
+                val radiobuttons1 = radiobuttons
+                  .get(index)
+                radiobuttons1
+                  .getElementsByClass(Selectors.radiosLabels)
+                  .text() mustBe Messages("organisationType." + radio1.toString)
+                val input = radiobuttons1
+                  .getElementsByClass(Selectors.radiosInput)
+                input.attr("value") mustBe radio1.toString
+                input.hasAttr("checked") mustBe false
+              }
+            }
+          }
+        }
+      }
+    }
+
+    "contain the correct button" - {
+      document.getElementsByClass(Selectors.button).text() mustBe Messages("site.continue")
+    }
+
+    "contains a form with the correct action" - {
+      "when in CheckMode" in {
+        val htmlAllSelected = view(form.fill(OrganisationType.values.head), CheckMode, withoutSoleTrader)(request, messages(application))
+        val documentAllSelected = doc(htmlAllSelected)
+
+        documentAllSelected.select(Selectors.form)
+          .attr("action") mustEqual routes.OrganisationTypeController.onSubmit(CheckMode).url
+      }
+
+      "when in NormalMode" in {
+        val htmlAllSelected = view(form.fill(OrganisationType.values.head), NormalMode, withoutSoleTrader)(request, messages(application))
+        val documentAllSelected = doc(htmlAllSelected)
+
+        documentAllSelected.select(Selectors.form)
+          .attr("action") mustEqual routes.OrganisationTypeController.onSubmit(NormalMode).url
+      }
+    }
+
+    "when there are form errors" - {
+      val htmlWithErrors = view(form.bind(Map("value" -> "")), NormalMode, withoutSoleTrader)(request, messages(application))
+      val documentWithErrors = doc(htmlWithErrors)
+
+      "should have a title containing error" in {
+        documentWithErrors.title must include("Error: Which of these best describes your business?")
+      }
+
+      "contains a message that links to field with error" in {
+        val errorSummary = documentWithErrors
+          .getElementsByClass(Selectors.errorSummaryList)
+          .first()
+        errorSummary
+          .select("a")
+          .attr("href") mustBe "#value_0"
+        errorSummary.text() mustBe "Select the option that best describes your business"
+      }
+    }
+
+    testBackLink(document)
+    validateTimeoutDialog(document)
+    validateTechnicalHelpLinkPresent(document)
+    validateAccessibilityStatementLinkPresent(document)
+  }
+
+}
