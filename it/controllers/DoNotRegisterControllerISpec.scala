42686ef234924ea5f13b52bb8ad7f2e333f4fe8a
Chris Hill <26716179+ChrisHillCap@users.noreply.github.com>
2023-05-26 10:54:02 +0100

    DLS-7707 wip

---
+++ it/controllers/DoNotRegisterControllerISpec.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import org.jsoup.Jsoup
+import org.scalatest.matchers.must.Matchers.{convertToAnyMustWrapper, include}
+import play.api.i18n.Messages
+import play.api.test.WsTestClient
+
+class DoNotRegisterControllerISpec extends ControllerITTestHelper {
+
+  val normalRoutePath = "/do-not-register"
+
+  "GET " + normalRoutePath - {
+    "should return OK and render the DoNotRegister page" in {
+      given
+        .commonPrecondition
+
+      setAnswers(emptyUserAnswers)
+
+      WsTestClient.withClient { client =>
+        val result1 = createClientRequestGet(client, baseUrl + normalRoutePath)
+
+        whenReady(result1) { res =>
+          res.status mustBe 200
+          val page = Jsoup.parse(res.body)
+          page.title must include(Messages("doNotRegister" + ".title"))
+        }
+      }
+    }
+    testOtherSuccessUserTypes(baseUrl + normalRoutePath, Messages("doNotRegister" + ".title"
+    ) )
+    testUnauthorisedUser(baseUrl + normalRoutePath)
+    testAuthenticatedUserButNoUserAnswers(baseUrl + normalRoutePath)
+  }
+}
