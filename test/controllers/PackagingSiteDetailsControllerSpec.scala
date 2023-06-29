758f0e4e7d9283ff1624a2b6c0b83cf84945570e
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2023-06-29 15:12:01 +0100

    [DLS-7947] Fixing tests

--- test/controllers/PackagingSiteDetailsControllerSpec.scala
+++ test/controllers/PackagingSiteDetailsControllerSpec.scala
@@ -209,34 +209,5 @@ class PackagingSiteDetailsControllerSpec extends SpecBase with MockitoSugar with
         redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
-
-    "should log an error message when internal server error is returned when user answers are not set in session repository" in {
-      val mockSessionService = mock[SessionService]
-
-      when(mockSessionService.set(any())) thenReturn Future.successful(Left(SessionDatabaseInsertError))
-
-      val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers), rosmRegistration = rosmRegistration)
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionService].toInstance(mockSessionService)
-          )
-          .build()
-
-      running(application) {
-        withCaptureOfLoggingFrom(application.injector.instanceOf[GenericLogger].logger) { events =>
-          val request =
-            FakeRequest(POST, packagingSiteDetailsRoute)
-          .withFormUrlEncodedBody(("value", "true"))
-
-          await(route(application, request).value)
-          events.collectFirst {
-            case event =>
-              event.getLevel.levelStr mustBe "ERROR"
-              event.getMessage mustEqual "Failed to set value in session repository while attempting set on packagingSiteDetails"
-          }.getOrElse(fail("No logging captured"))
-        }
-      }
-    }
   }
 }
