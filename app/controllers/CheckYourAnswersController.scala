34713a33ed3119e8bfde4e7a28f0d7c453bc45f6
Chris Hill <26716179+ChrisHillCap@users.noreply.github.com>
2023-07-14 16:12:16 +0100

    DLS-8351 added in guard to CYA (#57)

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -17,30 +17,39 @@
 package controllers
 
 import com.google.inject.Inject
-import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction, RequiredUserAnswers}
+import pages.CheckYourAnswersPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.CheckYourAnswersView
 import views.summary.RegistrationSummary
 
+import scala.concurrent.Future
+
 class CheckYourAnswersController @Inject()(
                                             override val messagesApi: MessagesApi,
                                             identify: IdentifierAction,
                                             getData: DataRetrievalAction,
                                             requireData: DataRequiredAction,
+                                            requiredUserAnswers: RequiredUserAnswers,
                                             val controllerComponents: MessagesControllerComponents,
                                             view: CheckYourAnswersView
                                           ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData) {
+  def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
-      val summaryList = RegistrationSummary.summaryList(request.userAnswers)
-      Ok(view(summaryList, routes.CheckYourAnswersController.onSubmit()))
+      requiredUserAnswers.requireData(CheckYourAnswersPage) {
+        val summaryList = RegistrationSummary.summaryList(request.userAnswers)
+        Future.successful(Ok(view(summaryList, routes.CheckYourAnswersController.onSubmit())))
+      }
   }
 
-  def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData) {
-    Redirect(controllers.routes.IndexController.onPageLoad().url)
+  def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      requiredUserAnswers.requireData(CheckYourAnswersPage) {
+        Future.successful(Redirect(controllers.routes.IndexController.onPageLoad().url))
+    }
   }
 }
 
