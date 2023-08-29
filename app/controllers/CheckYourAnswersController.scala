17e5f463c9ede6e7ab799be860431ff7066bf169
Ramin Esfandiari <ramin.esfan@gmail.com>
2023-08-29 16:54:27 +0100

    update production site and warehouse conditions
    DLS-8747: remove warehouse conditional remove on CYA
    
    DLS-8747: resolve merge conflicts
    
    DLS-8747: add more tests for large producer sites
    
    DLS-8747: update conditions of production site presentation
    
    DLS-8747: update conditions of small producer conditional display
    
    DLS-8747: more test cases for large producer cases
    
    DLS-8747: more test cases for large producer cases
    
    DLS-8747: more test cases for small producer cases
    
    DLS-8747: refactor test descriptions
    
    DLS-8747: add more tests for small producer cases
    
    DLS-8747: add more tests for none producers
    
    DLS-8747: cover edge case for small producer and warehouses
    
    DLS-8747: remove imports

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -50,7 +50,6 @@ class CheckYourAnswersController @Inject()(
             Future.successful(Ok(view(summaryList, routes.CheckYourAnswersController.onSubmit)))
           case Left(_) => Future.successful(Redirect(routes.RegistrationController.start))
         }
-
       }
   }
 
