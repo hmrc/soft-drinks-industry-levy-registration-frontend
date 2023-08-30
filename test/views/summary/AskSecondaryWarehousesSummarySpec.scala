06349d52f594113fba7d09fe2a0765161e296e72
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-08-30 12:01:35 +0100

    DLS-8681: AskSecondaryWarehouseSummary tests for both NormalMode and CheckMode

--- test/views/summary/AskSecondaryWarehousesSummarySpec.scala
+++ test/views/summary/AskSecondaryWarehousesSummarySpec.scala
@@ -19,7 +19,7 @@ package views.summary
 import base.SpecBase
 import controllers.routes
 import models.backend.UkAddress
-import models.{NormalMode, Warehouse}
+import models.{CheckMode, NormalMode, Warehouse}
 import uk.gov.hmrc.govukfrontend.views.Aliases.HtmlContent
 
 class AskSecondaryWarehousesSummarySpec extends SpecBase {
@@ -63,11 +63,86 @@ class AskSecondaryWarehousesSummarySpec extends SpecBase {
 
   "row2" - {
 
-  "should return an empty list of summaryListRows when no packaging site list is passed in" in {
-  val WarehouseDetailsSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map.empty, NormalMode)
+    List(NormalMode, CheckMode).foreach { mode =>
+      s"In ${mode.toString}" - {
+        "should return an empty list of summaryListRows when no packaging site list is passed in" in {
+          val WarehouseDetailsSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map.empty, mode)
 
-    WarehouseDetailsSummaryRowList mustBe List()
-}
+          WarehouseDetailsSummaryRowList mustBe List()
+        }
+
+        "must not return a remove action if only 1 packaging site is passed in" in {
+          val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(warehouseListWith1, mode)
+
+          warehouseSummaryRowList.mkString mustNot include("Remove")
+        }
+        "must include Correct elements in list with 2 elements" in {
+          val site1 = Warehouse(
+            None,
+            UkAddress(List("foo2", "bar2"), "wizz2"))
+          val site2 = Warehouse(
+            None,
+            UkAddress(List("foo", "bar"), "wizz"))
+          val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map("ref1" -> site1, "ref2" -> site2), mode)
+          warehouseSummaryRowList.head.key.content.asHtml.toString() mustBe "foo2, bar2, wizz2"
+          warehouseSummaryRowList.head.actions.toList.head.items.last.content.asHtml.toString() mustBe "Remove"
+          warehouseSummaryRowList.head.actions.toList.head.items.last.href mustBe routes.RemoveWarehouseDetailsController.onPageLoad(mode, "ref1").url
+
+          warehouseSummaryRowList.last.key.content.asHtml.toString() mustBe "foo, bar, wizz"
+          warehouseSummaryRowList.last.actions.toList.head.items.last.content.asHtml.toString() mustBe "Remove"
+          warehouseSummaryRowList.last.actions.toList.head.items.last.href mustBe routes.RemoveWarehouseDetailsController.onPageLoad(mode, "ref2").url
+        }
+        "address formatting within Row2" - {
+
+          "should place a break after a trading name if a trading name is used" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map(("90831480921", addressWith3AddressLines)), mode)
+            val expectedAddressContent = HtmlContent("The house, The Road, ugzhkxcajkcjfrqsgkjruzlmsxytwhg vdg, NW88 8II")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent
+          }
+
+          "should not place a break before the post code if the address line and post code length is 44 characters" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map(("1208934391", address44Characters)), mode)
+            val expectedAddressContent = HtmlContent("29 Station Rd, The Railyard, Cambridge, CB1 2FP")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent
+          }
+
+          "should place a break before the post code if the address line and post code length is between 45 and 49 characters" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(WarehouseListWith3, mode)
+            val expectedAddressContent45 = HtmlContent("29 Station Pl., The Railyard, Cambridge, <br>CB1 2FP")
+            val expectedAddressContent47 = HtmlContent("29 Station Place, The Railyard, Cambridge, <br>CB1 2FP")
+            val expectedAddressContent49 = HtmlContent("29 Station PlaceDr, The Railyard, Cambridge, <br>CB1 2FP")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent45
+            warehouseSummaryRowList.apply(1).key.content mustBe expectedAddressContent47
+            warehouseSummaryRowList.last.key.content mustBe expectedAddressContent49
+          }
+
+          "should not place a break before the post code if the address line and post code length is 50 characters" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map(("3489028394r", address50Characters)), mode)
+            val expectedAddressContent = HtmlContent("29 Station Place Dr, The Railyard, Cambridge, CB1 2FP")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent
+          }
+          "should autowrap and place a break before the post code if the address line and post code length is between 98 & 103 characters" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map(("45641351", WarehouseEvenLongerAddressNoTradeName)), mode)
+            val expectedAddressContent = HtmlContent("29 Station Rd, This address will auto wrap but not in postcode, it is 4 lines 103 char, Cambridge, <br>CB1 2FP")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent
+          }
+
+          "should place a break after a trading name AND autowrap and place a break before the post code if the address line " +
+            "and post code length is between 98 & 103 characters" in {
+            val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(Map(("56458678", WarehouseEvenLongerAddressWithTradeName)), mode)
+            val expectedAddressContent = HtmlContent("29 Station Rd, This address will auto wrap but not " +
+              "in postcode, it is 4 lines 103 char, Cambridge, <br>CB1 2FP")
+
+            warehouseSummaryRowList.head.key.content mustBe expectedAddressContent
+          }
+        }
+      }
+    }
 
   "must return a remove action if only 1 packaging site is passed in" in {
   val warehouseSummaryRowList = WarehouseDetailsSummary.warehouseDetailsRow(warehouseListWith1, NormalMode)
